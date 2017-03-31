2cb9778945d38941dc5aeb36e69ecccc2f8316dc
João Elvas <j.elvas@campus.fct.unl.pt>
2017-03-31 21:39:16 +0100

    Delivery v1

--- src/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
+++ src/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
@@ -11,16 +11,25 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.gson.Gson;
 
-@Path("/utils")
+@Path("tasks")
 public class ComputationResource {
 
+	private static final DatastoreService DATASTORE = DatastoreServiceFactory.getDatastoreService();
+	
 	private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+	
 	private final Gson g = new Gson();
+	
 	private static final DateFormat fmt = new SimpleDateFormat("yyy-MM-dd HH:mm:ss:SSSZ");
 	
 	public ComputationResource() {
@@ -35,14 +44,30 @@ public class ComputationResource {
 	}
 	
 	@POST
-	@Path("/compute")
+	@Path("/tokencleanup")
 	public Response executeComputeTask() {
 		LOG.fine("Starting to execute computation tasks.");
 		
 		try {
-			Thread.sleep(60*1000*5);
+			Query q = new Query("Token");
+			
+			PreparedQuery pq = DATASTORE.prepare(q);
+			
+			Iterable<Entity> i = pq.asIterable();
+			
+			for(Entity e: i) {
+				long expiration_time = (long) e.getProperty("expiration_time");
+				
+				if(expiration_time < System.currentTimeMillis()) {
+					DATASTORE.delete(e.getKey());
+				}
+				
+				LOG.fine("Deleted token created on " + (String) e.getProperty("creation_time"));
+				
+			}
+			
 		} catch (Exception e) {
-			LOG.logp(Level.SEVERE, this.getClass().getCanonicalName(), "executeComputeTak", "An exception has ocurred", e);
+			LOG.logp(Level.SEVERE, this.getClass().getCanonicalName(), "executeComputeTask", "An exception has ocurred", e);
 			return Response.serverError().build();
 		}
 		
@@ -50,10 +75,10 @@ public class ComputationResource {
 	}
 	
 	@GET
-	@Path("/compute")
+	@Path("/tokencleanup")
 	public Response triggerExecuteComputeTask() {
 		Queue queue = QueueFactory.getDefaultQueue(); 
-		queue.add(TaskOptions.Builder.withUrl("/rest/utils/compute"));
+		queue.add(TaskOptions.Builder.withUrl("/rest/tasks/tokencleanup"));
 		return Response.ok().build(); 
 		}
 	
